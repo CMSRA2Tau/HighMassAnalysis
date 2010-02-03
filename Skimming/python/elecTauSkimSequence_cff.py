1f1267dfe27e7f170fd6580480664a4c8ee37827
gurrola <>
2010-02-03 12:41:42 +0000

    *** empty log message ***

--- Skimming/python/elecTauSkimSequence_cff.py
+++ Skimming/python/elecTauSkimSequence_cff.py
@@ -1,8 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from HiMassTauAnalyzer.Skimming.tauSelector_cfi import *
-from HiMassTauAnalyzer.Skimming.elecSelector_cfi import *
-
+from HighMassAnalysis.Skimming.tauSelector_cfi import *
+from HighMassAnalysis.Skimming.elecSelector_cfi import *
 
 elecTauPairs = cms.EDProducer("DeltaRMinCandCombiner",
   decay = cms.string('selectedPFTaus@+ selectedElectrons@-'),
