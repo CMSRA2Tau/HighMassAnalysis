935607a9cc1c2b478382c24d797fd9fd4f3c0ee7
eluiggi <>
2010-12-03 17:49:20 +0000

    First commit of file controlling whether we are doing data/mc and which channel

---
+++ Configuration/python/hiMassSetup_cfi.py
@@ -0,0 +1,15 @@
+### This files controls whether we are working with data/MC, signalMC (for skim), and channel (not implemented yet).
+### Make sure to set it to the desired process.
+
+### signal refers to signal MC => data must be false when signal is true
+### data refers to data/mc analysis
+### channels: emu, etau, mutau
+global signal
+global data
+global channel
+
+signal = False
+data = True
+channel = "emu"
+
+
