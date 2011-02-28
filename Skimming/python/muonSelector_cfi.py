ec83a7e8506b2620d3984ec9b3a1d712876eeed4
meloam <>
2011-02-28 04:19:12 +0000

    This commit was manufactured by cvs2svn to create tag 'data'.
    Sprout from master 2011-02-28 04:19:11 UTC isuarez ''
    Delete:
        Analysis/BuildFile
        Analysis/interface/HiMassTauAnalysis.h
        Analysis/src/HiMassTauAnalysis.cc
        Analysis/test/eTau_NoCuts_cfg.py
        Analysis/test/muTau_acceptance_cfg.py
        Configuration/python/customizePAT_cff.py
        Configuration/python/hiMassSetup_cfi.py
        Configuration/python/metTools.py
        Configuration/python/patTupleEventContentForHiMassTau_cff.py
        Configuration/python/producePatTuple_cff.py
        Configuration/test/hiMassTau_patProd.py
        Skimming/python/EventContent_cff.py
        Skimming/python/TauTauSkimSequence_cff.py
        Skimming/python/elecSelector_cfi.py
        Skimming/python/elecTauSkimSequence_cff.py
        Skimming/python/genDecaysFromZPrimes_cfi.py
        Skimming/python/genLevelSequence_cff.py
        Skimming/python/genTauJetSelector_cfi.py
        Skimming/python/leptonLeptonSkimSequence_cff.py
        Skimming/python/leptonLeptonTauSkimSequence_cff.py
        Skimming/python/muTauSkimSequence_cff.py
        Skimming/python/muonSelector_cfi.py
        Skimming/python/tauSelector_cfi.py
        Skimming/python/triggerReq_cfi.py
        Skimming/test/allSkims_ForSignal_cfg.py
        Skimming/test/skimElecElec_ForBackground_cfg.py
        Skimming/test/skimElecElec_ForSignal_cfg.py
        Skimming/test/skimElecMu_ForBackground_cfg.py
        Skimming/test/skimElecMu_ForSignal_cfg.py
        Skimming/test/skimElecTau_ForBackground_cfg.py
        Skimming/test/skimElecTau_ForSignal_cfg.py
        Skimming/test/skimLeptonLeptonTau_cfg.py
        Skimming/test/skimMuMu_ForBackground_cfg.py
        Skimming/test/skimMuMu_ForSignal_cfg.py
        Skimming/test/skimMuTau_ForBackground_cfg.py
        Skimming/test/skimMuTau_ForSignal_cfg.py
        Skimming/test/skimTauTau_ForBackground_cfg.py
        Skimming/test/skimTauTau_ForSignal_cfg.py

--- Skimming/python/muonSelector_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-selectedMuons = cms.EDFilter("MuonSelector",
-  src = cms.InputTag('muons'),
-  cut = cms.string("isGlobalMuon & pt > 8 & abs(eta) < 2.5"),
-  filter = cms.bool(True)
-)
