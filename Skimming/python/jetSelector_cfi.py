b849ba374546edbfb4194a01252973a41e54013d
meloam <>
2012-05-25 17:08:21 +0000

    This commit was manufactured by cvs2svn to create tag 'for52X_v3'.
    Sprout from master 2012-05-25 17:08:19 UTC willhf 'Changed NVertices histogram range to 0-100'
    Delete:
        Configuration/python/patTupleEventContentForHiMassTau_cff.py
        Configuration/test/Data_TauTauSkim/JSON.txt
        Configuration/test/Data_TauTauSkim/crab_Data.cfg
        Configuration/test/Data_TauTauSkim/hiMassTau_patProd.py
        Fitter/Makefile
        Fitter/README.pdf
        Fitter/README.txt
        Fitter/TChProp.cc
        Fitter/TChProp.h
        Fitter/TFitter.cc
        Fitter/TFitter.h
        Fitter/TPrProp.cc
        Fitter/TPrProp.h
        Fitter/TSystematicStateGenerator.cc
        Fitter/TSystematicStateGenerator.h
        Fitter/channels.dat
        Fitter/muTau_Ntuple.root
        Fitter/runFitter.C
        Skimming/python/EventContent_cff.py
        Skimming/python/TauTauSkimSequence_cff.py
        Skimming/python/WHTauTauGenLevel_cfi.py
        Skimming/python/elecSelector_cfi.py
        Skimming/python/elecTauSkimSequence_cff.py
        Skimming/python/genDecaysFromZPrimes_cfi.py
        Skimming/python/genLevelSequence_cff.py
        Skimming/python/genTauJetSelector_cfi.py
        Skimming/python/jetSelector_cfi.py
        Skimming/python/leptonLeptonSkimSequence_cff.py
        Skimming/python/leptonLeptonTauSkimSequence_cff.py
        Skimming/python/muTauSkimSequence_cff.py
        Skimming/python/muonSelector_cfi.py
        Skimming/python/tauSelector_cfi.py
        Skimming/python/triggerReq_cfi.py
        Skimming/test/allSkims_ForSignal_cfg.py
        Skimming/test/skimElecElec_ForBackground_cfg.py
        Skimming/test/skimElecElec_ForSignal_cfg.py
        Skimming/test/skimElecMu_ForBackground_cfg.py
        Skimming/test/skimElecMu_ForSignal_cfg.py
        Skimming/test/skimElecTau_ForBackground_cfg.py
        Skimming/test/skimElecTau_ForSignal_cfg.py
        Skimming/test/skimLeptonLeptonTau_cfg.py
        Skimming/test/skimMuMu_ForBackground_cfg.py
        Skimming/test/skimMuMu_ForSignal_cfg.py
        Skimming/test/skimMuTau_ForBackground_cfg.py
        Skimming/test/skimMuTau_ForSignal_cfg.py
        Skimming/test/skimTauTau_ForBackground_cfg.py
        Skimming/test/skimTauTau_ForSignal_cfg.py
        Tags.csh
        Utilities/interface/SmearedElectronProducer.h
        Utilities/interface/SmearedMuonProducer.h
        Utilities/src/SmearedElectronProducer.cc
        Utilities/src/SmearedMuonProducer.cc
        Utilities/test/smearedmuonproducer_cfg.py

--- Skimming/python/jetSelector_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-selectedPatJets = cms.EDFilter("PATJetSelector",
-    src = cms.InputTag("patJets"),
-    cut = cms.string(''),
-    filter = cms.bool(True)
-)
